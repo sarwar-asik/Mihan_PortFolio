44dcb346e1c3ba13367333aaa47e8de98a39068e
sarwar-asik <shasik2021@gmail.com>
2023-02-27 14:53:14 +0600

    blogs

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,14 +5,18 @@ import styles from '@/styles/Home.module.css'
 import Navbar from './shared/Navbar/Navbar'
 import Header from './components/Header/Header'
 import Footer from './shared/Footer/Footer'
+import Experience from './components/experience/Experience'
+import Blogs from './components/blogs/Blogs'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-   <main className='container lg:w-[90%] sm:w-full mx-auto'>
+   <main className='container lg:w-[96%] sm:w-full mx-auto'>
     <Navbar/>
     <Header/>
+    <Experience/>
+    <Blogs/>
     <Footer/>
    </main>
   )
